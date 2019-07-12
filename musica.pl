9b143a37581044fd5c9691e4e518a4255bb124e5
lspigariol <lspigariol@gmail.com>
2019-07-12 15:19:52 -0300

    solucion

--- musica.pl
+++ musica.pl
@@ -16,3 +16,51 @@ manager(rodrigoMalo, estafador).
 % normal(porcentajeComision) 
 % buenaOnda(nombre, lugar)
 % estafador     
+
+
+porcentaje(canada, 5).
+porcentaje(mexico,1.5).
+
+clasico(Artista) :-
+  disco(Artista, loMejorDe, _, _).
+clasico(Artista) :-
+  disco(Artista, _, Ventas, _),
+  Ventas > 1000000.
+
+ventasTotales(Artista, Ventas) :-
+	disco(Artista, _, _, _),
+	findall(Ventas, disco(Artista, _, Ventas, _), ListaDeVentas),
+	sum_list(ListaDeVentas, Ventas).
+
+ventasBrutas(Artista, Bruto) :-
+  ventasTotales(Artista, Total),
+  Bruto is Total/10.
+
+ventasNetas(Artista, Neto) :-
+  ventasBrutas(Artista, Bruto),
+  manager(Artista, Manager),
+  descuento(Manager,Descuento),
+  porcentaje(Descuento, Bruto, Neto).
+
+ventasNetas(Artista, Bruto) :-
+    ventasBrutas(Artista, Bruto),
+    not(manager(Artista, _)).
+
+descuento(buenaOnda(_,Pais), Descuento) :-
+	porcentaje(Pais, Descuento).
+descuento(estafador,100).
+descuento(normal(Descuento), Descuento). 
+
+porcentaje(Porcentaje, Bruto, Neto):-
+	Neto is Bruto*(100-Porcentaje).
+
+numberUan(Anio, Artista) :-
+  artistaAutogestionado(Artista),
+  disco(Artista, _, Cantidad, Anio),
+  not((disco(OtroArtista, _, OtraCantidad, Anio),
+	artistaAutogestionado(OtroArtista),
+	Cantidad < OtraCantidad)).
+
+artistaAutogestionado(Artista) :-
+  disco(Artista, _, _, _),
+  not(manager(Artista, _)).
